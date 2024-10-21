7f8c8cffcc901468ab31e8d16db894af2b3744a7
Haejeong079 <goesed7582@naver.com>
2024-10-21 15:57:01 +0900

    Security Config

--- src/main/java/inhatc/cse/yhjshope/config/SecurityConfig.java
+++ src/main/java/inhatc/cse/yhjshope/config/SecurityConfig.java
@@ -3,13 +3,14 @@ package inhatc.cse.yhjshope.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
--- src/main/java/inhatc/cse/yhjshope/member/repository/MemberRepository.java
+++ src/main/java/inhatc/cse/yhjshope/member/repository/MemberRepository.java
@@ -2,13 +2,13 @@ package inhatc.cse.yhjshope.member.repository;
 
 import inhatc.cse.yhjshope.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Optional<Member> findByEmail(String email);
-
-
 
 }
