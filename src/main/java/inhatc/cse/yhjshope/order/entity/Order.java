0acbcf73566a1b84369bbfa39e3e4dd304eb0b21
Haejeong079 <goesed7582@naver.com>
2024-11-12 10:58:38 +0900

    11/12
    Order, OrderRepository

--- src/main/java/inhatc/cse/yhjshope/order/entity/Order.java
+++ src/main/java/inhatc/cse/yhjshope/order/entity/Order.java
@@ -9,6 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Orders") //데이터베이스 특성상 Order -> Orders 이름을 적는게 더 적합
@@ -22,6 +24,10 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Order_id")
     private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "order",
+            cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
